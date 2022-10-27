bf6e8b542c71ad2082b6b4e7da8ce8bb752e6dfb
Sceat <fetch@sceat.xyz>
2022-10-28 00:18:21 +0900

    chore: optimize images

--- src/components/page_5.mobile.vue
+++ src/components/page_5.mobile.vue
@@ -19,14 +19,14 @@
     min-height 30vh
     box-shadow 1px 2px 3px black
   :nth-child(1)
-    background url('src/assets/s1.png') center / cover
+    background url('src/assets/s1.jpeg') center / cover
     z-index 4
   :nth-child(2)
-    background url('src/assets/s2.png') center / cover
+    background url('src/assets/s2.jpeg') center / cover
     z-index 3
   :nth-child(3)
-    background url('src/assets/s3.png') center / cover
+    background url('src/assets/s3.jpeg') center / cover
     z-index 2
   :nth-child(4)
-    background url('src/assets/s4.png') center / cover
+    background url('src/assets/s4.jpeg') center / cover
 </style>
